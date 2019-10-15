2280ed9cf8bf8a5bdcfc733bbaeed5a9c185331d
Caner Yazıcı <contact@iyzico.com>
2019-10-15 20:06:07 +0300

    settlement to balance, refund to balance, pay with iyzico added to client

---
+++ spec/refund_to_balance_spec.rb
@@ -0,0 +1,37 @@
+# coding: utf-8
+require_relative 'spec_helper'
+
+RSpec.describe 'Iyzipay' do
+  before :all do
+    @options = Iyzipay::Options.new
+    @options.api_key = SpecOptions::API_KEY
+    @options.secret_key = SpecOptions::SECRET_KEY
+    @options.base_url = SpecOptions::BASE_URL
+  end
+
+  it 'should refund to balance payment' do
+    # create payment
+
+    request = {
+        locale: Iyzipay::Model::Locale::TR,
+        conversationId: '123456789',
+        paymentId: '11661247',
+        callbackUrl: 'https://merchanturl.com'
+    }
+    refundToBalance = Iyzipay::Model::RefundToBalance.new.create(request, @options)
+    begin
+      $stdout.puts refundToBalance.inspect
+      refundToBalance = JSON.parse(refundToBalance)
+      puts refundToBalance.inspect
+      expect(refundToBalance['status']).to eq('success')
+      expect(refundToBalance['token']).not_to be_nil
+      expect(refundToBalance['url']).not_to be_nil
+    rescue
+      $stderr.puts 'oops'
+      raise
+    end
+  end
+
+  after :each do
+  end
+end
---
+++ spec/settlement_to_balance_spec.rb
@@ -0,0 +1,39 @@
+# coding: utf-8
+require_relative 'spec_helper'
+
+RSpec.describe 'Iyzipay' do
+  before :all do
+    @options = Iyzipay::Options.new
+    @options.api_key = SpecOptions::API_KEY
+    @options.secret_key = SpecOptions::SECRET_KEY
+    @options.base_url = SpecOptions::BASE_URL
+  end
+
+  it 'should make settlement to balance' do
+    request = {
+        subMerchantKey: 'qGtsC8HgrrJlH3acAcxT0M3ls+U=',
+        callbackURL: 'https://merchantwebsite.com',
+        price: '10',
+        locale: 'tr',
+        conversationId: '123456'
+    }
+    settlement_to_balance = Iyzipay::Model::SettlementToBalance.new.create(request, @options)
+    begin
+      $stdout.puts settlement_to_balance.inspect
+      puts settlement_to_balance = JSON.parse(settlement_to_balance)
+      expect(settlement_to_balance['status']).to eq('success')
+      expect(settlement_to_balance['locale']).to eq('tr')
+      expect(settlement_to_balance['systemTime']).not_to be_nil
+      expect(settlement_to_balance['conversationId']).to eq('123456')
+      expect(settlement_to_balance['url']).not_to be_nil
+      expect(settlement_to_balance['token']).not_to be_nil
+      expect(settlement_to_balance['isSettingsAllTime']).not_to be_nil
+    rescue
+      $stderr.puts 'oops'
+      raise
+    end
+  end
+
+  after :each do
+  end
+end
