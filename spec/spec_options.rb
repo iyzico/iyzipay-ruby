59e0658e77ac713a8a3885ed790a46be88a4aad7
can3ry <51880555+can3ry@users.noreply.github.com>
2019-10-16 10:33:06 +0300

    apikey changed

--- spec/spec_options.rb
+++ spec/spec_options.rb
@@ -1,5 +1,5 @@
 module SpecOptions
-  API_KEY = 'sandbox-Bu3x4iFM7POxAGxwVR8at3VGjZbeqlgH'
-  SECRET_KEY = 'sandbox-Gkyc1uOkduM9HVIe9cLFiaq7nt7WpAXZ'
+  API_KEY = 'sandbox-afXhZPW0MQlE4dCUUlHcEopnMBgXnAZI'
+  SECRET_KEY = 'sandbox-wbwpzKIiplZxI3hh5ALI4FJyAcZKL6kq'
   BASE_URL = 'https://sandbox-api.iyzipay.com'
 end
